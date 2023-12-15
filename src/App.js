8b79a8afdce4047ffb1c19705b6c93ff6ac7c375
Elyse Montano <elysemontano.dev.com>
2023-12-15 14:25:49 -0800

    adds connecting signup, signin, logout to api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 import mockUsers from "./mockUsers.js"
 import mockApartments from "./mockApartments.js"
@@ -16,19 +16,89 @@ import Signup from "./pages/SignUp"
 import Login from "./pages/SignIn"
 
 const App = () => {
-  const [currentUser, setCurrentUser] = useState(mockUsers[0])
+  const [currentUser, setCurrentUser] = useState(null)
   const [apartments, setApartments] = useState(mockApartments)
 
-  // console.log("currentUser", currentUser)
+  console.log("currentUser", currentUser)
   // console.log("apartments", apartments)
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user")
+    if(loggedInUser) {
+      setCurrentUser(JSON.parse(loggedInUser))
+    }
+  }, [])
+
+  const login = (userInfo) => {
+    fetch(`http://localhost:3000/login`, {
+      body: JSON.stringify(userInfo),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        // store the token
+        localStorage.setItem("token", response.headers.get("Authorization"))
+        return response.json()
+      })
+      .then((payload) => {
+        localStorage.setItem("user", JSON.stringify(payload))
+        setCurrentUser(payload)
+      })
+      .catch((error) => console.log("login errors: ", error))
+  }
+
+  const signUp = (userInfo) => {
+    fetch(`http://localhost:3000/signup`, {
+      body: JSON.stringify(userInfo),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        // store the token
+        localStorage.setItem("token", response.headers.get("Authorization"))
+        return response.json()
+      })
+      .then((payload) => {
+        localStorage.setItem("user", JSON.stringify(payload))
+        setCurrentUser(payload)
+      })
+      .catch((error) => console.log("login errors: ", error))
+  }
+
+  const logout = () => {
+    fetch(`http://localhost:3000/logout`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"), //retrieve the token
+      },
+      method: "DELETE",
+    })
+      .then((payload) => {
+        localStorage.removeItem("token") // remove the token
+        setCurrentUser(null)
+      })
+      .catch((error) => console.log("log out errors: ", error))
+  }
 
   return (
     <>
-      <Header currentUser={currentUser}/>
+      <Header currentUser={currentUser} logout={logout}/>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login login={login} />} />
+        <Route path="/signup" element={<Signup signUp={signUp} />} />
         <Route path="/apartmentindex" element={<ApartmentIndex apartments={apartments}/>} />
         {
           currentUser && <Route path="/myapartments" element={<ApartmentProtectedIndex currentUser={currentUser} apartments={apartments} />} />
